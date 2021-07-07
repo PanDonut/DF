a9dd4effe6e9c46241b82270e3a631f22e615969
PanDonut <81489726+PanDonut@users.noreply.github.com>
2021-07-07 19:08:52 +0200

    Update login.js

--- scripts/login.js
+++ scripts/login.js
@@ -1,7 +1,6 @@
 function check(form) {
-	username = new Array("admin1", "admin2", "Mistfall", "Jendros", "PanDonut");
-	password = new Array("admin1", "admin2", "mPMql", "IvafB", "rura");
-
+	username = new Array("admin1", "admin2", "Mistfall", "Jendros", "PanDonut", "Hubert");
+	password = new Array("admin1", "admin2", "mPMql", "IvafB", "rura", "hubert");
 	if(form.userid.value == username[0] && form.pswrd.value == password[0]||form.userid.value == username[1] && form.pswrd.value == password[1]) {
 		location.replace('#adminhome')/* The Administrator homepage */
 	}
@@ -12,6 +11,9 @@ function check(form) {
 		location.replace('https://app.duckfilms.cloudns.ph/load/')/* The members homepage */
 	}
 	else if (form.userid.value == username[4] && form.pswrd.value == password[4]) {
+		location.replace('https://app.duckfilms.cloudns.ph/load/')/* The members homepage */
+	}
+	else if (form.userid.value == username[5] && form.pswrd.value == password[5]) {
 		location.replace('https://app.duckfilms.cloudns.ph/load/')/* The members homepage */
 	}
 	else if(form.userid.value == username[0]||form.userid.value == username[1]||form.userid.value == username[2]) {

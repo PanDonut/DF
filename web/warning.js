c5b29409495ab3d131a9c42216ef79bc41850ce5
PanDonut <krystian.wrobel06@gmail.com>
2021-08-26 23:14:09 +0200

    app

--- web/warning.js
+++ web/warning.js
@@ -4,12 +4,12 @@ const warningDescCSS = 'font-size: 20px;';
 
 const infoDescCSS = 'color:gray; font-family: "Arial Black", Gadget, sans-serif; font-size: 16px; font-weight: 100; font-style: normal;';
 
-console.log('%cUwaga!', warningTitleCSS);
+console.warn('%cUwaga!', warningTitleCSS);
       console.log("%cJeżeli ktoś powiedział Ci żeby coś tu wkleić to na 99.9% próbuje Cię oszukać. Wklejając tu coś ktoś może uzyskać dostęp do twojego konta lub urządzenia.", warningDescCSS);
       console.log("%cJeżeli doskonale wiesz co robisz dołącz do TheDuck Studios 😃", infoDescCSS);
 
 window.setInterval(function() {
-      console.log('%cUwaga!', warningTitleCSS);
+      console.warn('%cUwaga!', warningTitleCSS);
       console.log("%cJeżeli ktoś powiedział Ci żeby coś tu wkleić to na 99.9% próbuje Cię oszukać. Wklejając tu coś ktoś może uzyskać dostęp do twojego konta lub urządzenia.", warningDescCSS);
       console.log("%cJeżeli doskonale wiesz co robisz dołącz do TheDuck Studios 😃", infoDescCSS);
 }, 20000); // 20000 milisekund (20 sekund)

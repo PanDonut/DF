5dcf836a0f67b097ff9c8ea820f3501ab40bdc72
PanDonut <krystian.wrobel06@gmail.com>
2021-08-29 21:28:02 +0200

    app

--- service-worker.js
+++ service-worker.js
@@ -1,39 +1,10 @@
-'use strict';
-
-var cacheVersion = 1;
-var currentCache = {
-  offline: 'offline-cache' + cacheVersion
-};
-const offlineUrl = 'offline-page.html';
-
-this.addEventListener('install', event => {
+window.addEventListener("offline", function() {
   event.waitUntil(
-    caches.open(currentCache.offline).then(function(cache) {
+    caches.open('v1').then(function(cache) {
       return cache.addAll([
-          './img/offline.svg',
-          offlineUrl
+        'offline.png',
+        'offline-page.html',
       ]);
     })
   );
-});
-
-this.addEventListener('fetch', event => {
-  // request.mode = navigate isn't supported in all browsers
-  // so include a check for Accept: text/html header.
-  if (event.request.mode === 'navigate' || (event.request.method === 'GET' && event.request.headers.get('accept').includes('text/html'))) {
-        event.respondWith(
-          fetch(event.request.url).catch(error => {
-              // Return the offline page
-              return caches.match(offlineUrl);
-          })
-    );
-  }
-  else{
-        // Respond with everything else if we can
-        event.respondWith(caches.match(event.request)
-                        .then(function (response) {
-                        return response || fetch(event.request);
-                    })
-            );
-      }
 });

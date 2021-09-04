c23c4ac3c20884a24e0d3f3d8e807aeebd9cc5bc
PanDonut <krystian.wrobel06@gmail.com>
2021-09-04 22:55:06 +0200

    Update log.js

--- web/log.js
+++ web/log.js
@@ -3,11 +3,10 @@ const sess = localStorage.getItem('sesss');
 var ua = navigator.userAgent.toLowerCase();
 var isAndroid = ua.indexOf("android") > -1; //&& ua.indexOf("mobile");
 if(isAndroid) {
+} else {
     if(sess == 'false') {
     window.location.replace("../");
     } else if(sess == null) {
     localStorage.setItem('sesss', 'false');
     }
-} else {
-    
 }
